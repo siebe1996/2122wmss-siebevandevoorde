1cf8195e2ea3dcdddf9b67ea5d9a616ae7d44377
siebe <siebe.vandevoorde@student.odisee.be>
2021-10-06 12:21:54 +0200

    labo 3 oef 1,2

--- app/public/labo01/bedrijven/bedrijven.php
+++ app/public/labo01/bedrijven/bedrijven.php
@@ -118,16 +118,19 @@ if (array_key_exists('sort', $_GET) && ($_GET['sort'] === 'zip')){
     <title>Voka - bedrijfslijst</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <!--[if lte IE 8]><script src="js/ie/html5shiv.js"></script><![endif]-->
+    <!--[if lte IE 8]>
+    <script src="js/ie/html5shiv.js"></script><![endif]-->
     <link rel="stylesheet" href="css/main.css" />
-    <!--[if lte IE 8]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="css/ie8.css"/><![endif]-->
 
     <!-- Scripts -->
     <script src="js/jquery.min.js"></script>
     <script src="js/jquery.poptrox.min.js"></script>
     <script src="js/skel.min.js"></script>
     <script src="js/util.js"></script>
-    <!--[if lte IE 8]><script src="js/ie/respond.min.js"></script><![endif]-->
+    <!--[if lte IE 8]>
+    <script src="js/ie/respond.min.js"></script><![endif]-->
     <script src="js/main.js"></script>
 </head>
 <body id="top">
